1f4789ba8b30854403a7686c71793ee44cb6de81
Sergio <sergio.pm.developer@gmail.com>
2024-02-27 23:14:47 +0000

    ops: initialize repository with project skeleton

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Link Loom',
+  description: 'Show yourself to the world through links',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='en'>
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
